b5dd7a7b7b67d8896f76fc4cc95820060f7b0bf6
ednomerve <ednomerve@6c450836-be4a-11dd-9405-3d0920048215>
2008-12-21 22:47:33 +0000

    optimized treestobushes.lua by using the number of objects on a tile opposed to using MAX_MAP_TILE_OBJECTS

--- tibialua/release/scripts/treestobushes.lua
+++ tibialua/release/scripts/treestobushes.lua
@@ -4,29 +4,36 @@ tibia_setstatusbartext("Replacing trees with bushes...")
 local mapBegin = tibia_getmappointer()
 local mapEnd   = mapBegin + (STEP_MAP_TILE * MAX_MAP_TILES)
 
+-- number of trees replaced
+local numTreesReplaced = 0
+
 -- search through map
 for i = mapBegin, mapEnd, STEP_MAP_TILE do
-    local j = 0
+    -- get tile object count address
+    local tileObjectCountAddress = i + OFFSET_MAP_TILE_OBJECT_COUNT
+
+    -- get tile object count
+    local tileObjectCount = tibia_readbytes(tileObjectCountAddress, 4)
 
     -- search through map objects on tile
-    while j < MAX_MAP_TILE_OBJECTS do
+    for j = 0, tileObjectCount do
         -- get object id address
         local objectIdAddress = i + OFFSET_MAP_OBJECT_ID + (j * STEP_MAP_OBJECT)
 
         -- get object id
-        local objectId = tibia_readbytes(objectIdAddress, 2)
+        local objectId = tibia_readbytes(objectIdAddress, 4)
 
         -- check if object id is a tree
         for k = 1, table.getn(OBJECT_TREES) do
             if objectId == OBJECT_TREES[k] then
                 -- replace object id with a bush
-                tibia_writebytes(objectIdAddress, OBJECT_BUSH, 2);
+                tibia_writebytes(objectIdAddress, OBJECT_BUSH, 4);
+
+                -- increment number of trees replaced
+                numTreesReplaced = numTreesReplaced + 1
             end
         end
-
-        -- next map object on tile
-        j = j + 1
     end
 end
 
-tibia_setstatusbartext("Trees To Bushes")
+tibia_setstatusbartext("Trees To Bushes! Replaced " .. numTreesReplaced .. " tree(s) with bushes.")
